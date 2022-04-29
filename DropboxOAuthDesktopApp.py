f9e9b28406913a33a1e06d48477afa84a9d101d4
leire <leiregarciaupv@gmail.com>
2022-04-29 10:55:38 +0200

    local_server() terminado

--- DropboxOAuthDesktopApp.py
+++ DropboxOAuthDesktopApp.py
@@ -5,21 +5,75 @@ import socket
 import json
 
 
-app_key =
-app_secret =
-server_addr =
-server_port =
+app_key = 'ya2ra1avjd5yiug'
+app_secret = 'tvejc2pnlh59fr5'
+server_addr = 'localhost'
+server_port ='8090'
 redirect_uri = "http://" + server_addr + ":" + str(server_port)
 
 
 def local_server():
-    # sartu kodea hemen
+    print('OAuth 2.0 access tokens lortzen...')
+    base_uri = "https://www.dropbox.com/oauth2/authorize"
+    goiburuak = {'Host': 'dropbox.com'}
+    datuak = {'client_id': app_key,
+              'redirect_uri': redirect_uri,  # Dirección IP de bucle invertido
+              'response_type': 'code', }
+
+    datuak_kodifikatuta = urllib.parse.urlencode(datuak)  # parametroak URI-an daude
+    auth_uri = base_uri + '?' + datuak_kodifikatuta
+
+    print("\n\t Web nabigatzailea irekitzen: Dropbox prompts user for consent")
+    webbrowser.open_new(auth_uri)
+
+    print("\n\tHandle the OAuth 2.0 server response")
+    # 8090. portuan entzuten dagoen zerbitzaria sortu
+    listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    listen_socket.bind(('localhost', 8090))
+    listen_socket.listen(1)
+    print("\t\tSocket listening on port 8090")
+
+    # nabitzailetik 302 eskaera jaso beharko du
+    # ondorengo lerroan programa gelditzen da zerbitzariak 302 eskaera jasotzen duen arte
+    client_connection, client_address = listen_socket.accept()
+    eskaera = client_connection.recv(1024).decode()
+    print("\t\tNabigatzailetik ondorengo eskaera jaso da:")
+    print("\n" + eskaera)  # zerbitzariak jasotzen duen eskaera
+
+    # eskaeran "auth_code"-a bilatu
+    lehenengo_lerroa = eskaera.split('\n')[0]
+    aux_auth_code = lehenengo_lerroa.split(' ')[1]
+    auth_code = aux_auth_code[7:].split('&')[0]
+    print("auth_code: " + auth_code)
+
+    # erabiltzaileari erantzun bat bueltatu
+    http_response = """\
+    HTTP/1.1 200 OK
+
+    <html>
+    <head><title>Proba</title></head>
+    <body>
+    The authentication flow has completed. Close this window.
+    </body>
+    </html>
+    """
+    client_connection.sendall(str.encode(http_response))
+    client_connection.close()
+
     return auth_code
 
 
 def do_oauth():
     # Authorization
-    # sartu kodea hemen
+    print("###################################")
+    print("OAuth 2.0 for Mobile & Desktop Apps")
+    print("###################################")
+
+    print("\nPrerequisites on DropBox")
+    print("\tEnable APIs for your project")
+    print("\tChoose from different scopes in the ‘Permissions’ tab")
+    print("\tCreate authorization credentials")
 
     auth_code = local_server()
 
@@ -28,6 +82,7 @@ def do_oauth():
 
     return access_token
 
+'''
 
 def list_folder(access_token, cursor="", edukia_json_entries=[]):
     if not cursor:
@@ -50,3 +105,6 @@ def list_folder(access_token, cursor="", edukia_json_entries=[]):
 
 access_token = do_oauth()
 list_folder(access_token)
+
+'''
+access_token = do_oauth()
